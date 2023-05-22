12d379faa6e39dfccf3f7ba85f9e95140c0d37de
TanvirulAlamTusher <tanvirulalam15@gmail.com>
2023-05-22 17:48:47 +0600

    message

--- assainment-14/routes/web.php
+++ assainment-14/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DemoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+//Answer 1:
+Route::post('/name',[DemoController::class,'getName']);
+//Answer 2:
+Route::post('/useragent',[DemoController::class,'getUserAgent']);
+//Answer 5:
+Route::post('/upload', [DemoController::class, 'upload']);
+
+//Answer 6:
+Route::get('/cookie', [DemoController::class, 'getCookie']);
+
+//Answer 7:
+Route::post('/submit', [DemoController::class, 'submit']);
